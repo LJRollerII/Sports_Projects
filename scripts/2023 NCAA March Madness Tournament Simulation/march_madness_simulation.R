64d6100d57cc1f90e78d842369e7f54d0daf7774
LJRollerII <ljcoolkid@optonline.net>
2023-02-10 21:57:09 -0500

    Created outline for simulation. Now we just need to wait until Selection Sunday to fill in the rest of the code.

---
+++ scripts/2023 NCAA March Madness Tournament Simulation/march_madness_simulation.R
@@ -0,0 +1,90 @@
+#First Round
+
+#East
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+
+#West
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+
+#Midwest
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+
+#South
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+
+#Second Round
+
+#East
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+
+#West
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+
+#Midwest
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+
+#South
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+
+#Sweet 16
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+
+#Elite 8 
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+
+#Final Four 
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+
+#National Championship
+sample(c('',''), size = 1, prob = c(,))
