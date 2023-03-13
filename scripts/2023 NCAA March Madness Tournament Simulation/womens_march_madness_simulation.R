90a205fe277dba28f3c536418cfe7d9b6db452cf
LJRollerII <ljcoolkid@optonline.net>
2023-03-12 23:05:55 -0400

    Added power rating for both men's and women's tournament simulations

---
+++ scripts/2023 NCAA March Madness Tournament Simulation/womens_march_madness_simulation.R
@@ -0,0 +1,90 @@
+#First Round
+
+#Greenville 1
+sample(c('South Carolina',' Norfolk St'), size = 1, prob = c(1.0, 0.0))
+sample(c('South Florida','Marquette'), size = 1, prob = c(.43, .57))
+sample(c('Oklahoma','Portland'), size = 1, prob = c(.83 ,.17))
+sample(c('UCLA','Sacramento State'), size = 1, prob = c(.98, .02))
+sample(c('Creighton',''), size = 1, prob = c(.70, .30))
+sample(c('Notre Dame','Southern Utah'), size = 1, prob = c(.99, .01))
+sample(c('Arizona','West Virginia'), size = 1, prob = c(.65, .35))
+sample(c('Maryland','Holy Cross'), size = 1, prob = c(1.0, 0.0))
+
+#Seattle 4
+sample(c('Stanford',''), size = 1, prob = c(1.0, 0.0))
+sample(c('Ole Miss','Gonzaga'), size = 1, prob = c(.65, .35))
+sample(c('Louisville','Drake'), size = 1, prob = c(.78, .22))
+sample(c('Texas','East Carolina'), size = 1, prob = c(.98, .02))
+sample(c('Colorado','Middle Tennessee'), size = 1, prob = c(.70, .30))
+sample(c('Duke','Iona'), size = 1, prob = c(1.0, 0.0))
+sample(c('Florida State','Georgia'), size = 1, prob = c(.53, .47))
+sample(c('Iowa','Southeastern Louisiana'), size = 1, prob = c(1.0, 0.0))
+
+#Greenville 2
+sample(c('Indiana',''), size = 1, prob = c(1.0, 0.0))
+sample(c('Oklahoma State','Miami'), size = 1, prob = c(.58, .42))
+sample(c('Washington State',' Florida Gulf Coast'), size = 1, prob = c(.69, .31))
+sample(c('Villanova','Cleveland State'), size = 1, prob = c(.94, .06))
+sample(c('Michigan','UNLV'), size = 1, prob = c(.76, .24))
+sample(c('LSU','Hawaii'), size = 1, prob = c(1.0, 0.0))
+sample(c('NC State','Princeton'), size = 1, prob = c(.63, .37))
+sample(c('Utah','Gardner-Webb'), size = 1, prob = c(1.0, 0.0))
+
+#Seattle 3
+sample(c('Virginia Tech','Chattanooga'), size = 1, prob = c(1.0, 0.0))
+sample(c('USC','South Dakota State'), size = 1, prob = c(.61, .39))
+sample(c('Iowa State','Toledo'), size = 1, prob = c(.85, .15))
+sample(c('Tennessee','Saint Louis'), size = 1, prob = c(.99, .01))
+sample(c('North Carolina',''), size = 1, prob = c(.70, .30))
+sample(c('Ohio State','James Madison'), size = 1, prob = c(.99, .01))
+sample(c('Baylor','Alabama'), size = 1, prob = c(.57, .43))
+sample(c('UConn','Vermont'), size = 1, prob = c(1.0, 0.0))
+
+#Second Round
+
+#Greenville 1
+sample(c('',''), size = 1, prob = c(., .))
+sample(c('',''), size = 1, prob = c(., .))
+sample(c('',''), size = 1, prob = c(., .))
+sample(c('',''), size = 1, prob = c(., .))
+
+#Seattle 4
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+
+#Greenville 2
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+
+#Seattle 3
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+
+#Sweet 16
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+
+#Elite 8 
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+
+#Final Four 
+sample(c('',''), size = 1, prob = c(,))
+sample(c('',''), size = 1, prob = c(,))
+
+#National Championship
+sample(c('',''), size = 1, prob = c(,))
